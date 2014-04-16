096a31b47a24c5ef12c165fd5b12062b24c35781
Anya Priestley <anyapriestley@Anyas-MacBook-Air.local>
2014-04-16 18:28:59 -0400

    can actually type

--- rte.js
+++ rte.js
@@ -1,6 +1,6 @@
 $(function() {
     
-    var contents = [{
+    /*var contents = [{
         type: 'character',
         value: 'w',
         format: {
@@ -111,7 +111,9 @@ $(function() {
             bold: true,
             italic: true
         }
-    }];
+    }];*/
+
+    var contents = [];
 
     function render() {
         var letters = [],
@@ -137,91 +139,124 @@ $(function() {
         $('#editor').click(function (e) {
             e.stopPropagation();
         }); 
-
-        $.each(contents, function () {
-            var content = this,
-                contentVal = content.value;
-                idNum++;
-                var newSpan = $('<span></span>');
-            if (content.type == 'character') {
-                var format = content.format,
-                    bolded = format.bold,
-                    italicized = format.italic;
-                if (bolded && italicized) {
-                    newSpan.attr('id' , idNum);
-                    newSpan.html('<b><i>' + contentVal + '</i></b>');
-                    $("#editor").append(newSpan);
-                } else if (bolded) {
-                    newSpan.attr('id' , idNum);
-                    newSpan.html('<b>' + contentVal + '</b>');
-                    $("#editor").append(newSpan);
-                } else if (italicized) {
-                   newSpan.attr('id' , idNum);
-                    newSpan.html('<i>' + contentVal + '</i>');
-                    $("#editor").append(newSpan);
-                } else {
-                        newSpan.attr('id' , idNum);
-                        newSpan.html(contentVal);
-                        $("#editor").append(newSpan);
-                       }
-            } else {
-                newSpan.attr('id' , idNum + '_break');
-                newSpan.html(' <br> ');
-                $("#editor").append(newSpan);
-            }
-        });
-        
-        var lastSpan = $('<span></span>');
-        lastSpan.attr('id' , contents.length + 1 + '_last');
-        lastSpan.html('&nbsp;');
-        $("#editor").append(lastSpan);
-       
-        var letterSpans = $('#editor').children();
-        letterSpans.each(function () {
-            var letterSpan = $(this),
-                selectionStart,
-                selectionEnd,
-                difference;
-            letterSpan.mousedown(function () {
+        if(contents.length > 0){
+            $('#cursor').remove();
+            show();
+            contents = [];
+        } else {
+            $('#editor').click(function(){
                 if ($('#cursor')) {
                     $('#cursor').remove();
                 }
-                letterSpans.css('background-color' , 'transparent');
-                letterSpans.off('mousemove');
-                selectionStart = parseInt($(this).attr('id'));
-                letterSpans.off('mousemove').mousemove(function (ev) {
-                    letterSpans.off('mouseup').mouseup(function () {
-                    selectionEnd = parseInt($(this).attr('id'));
-                        difference = selectionStart - selectionEnd;
-                        for(var ww = 0; ww < Math.abs(difference); ww++){
-                           var spans = $('span');
-                            if(difference > 0){
-                               var redId = selectionEnd + ww;
-                            } else {
-                               var redId = selectionStart + ww;     
-                            }
-                           var red = $('#'+ redId);
-                           red.css('background-color', 'red'); 
-                        };
-                        
-                        letterSpans.off('mousemove');
+                cursor = $('<span>|</span>');
+                cursor.attr('id', 'cursor');
+                cursor.css({
+                    'color': 'red',
+                    'font-weight': 'bold'
+                   });
+                $('#editor').append(cursor);
+                setInterval(function () {
+                    cursorBlink()
+                }, 300);
+            });
+            if($('#cursor')){
+                $('#editor').keypress(function(ev){
+                    contents.push({
+                        type: 'character',
+                        value: String.fromCharCode(ev.keyCode),
+                        format: {bold:false, italic:false}
                     });
+                    render();
                 });
-                letterSpans.off('mouseup').mouseup(function () {
+            }
+        }
+
+        function show(){
+            $.each(contents, function () {
+                var content = this,
+                    contentVal = content.value;
+                    idNum++;
+                    var newSpan = $('<span></span>');
+                if (content.type == 'character') {
+                    var format = content.format,
+                        bolded = format.bold,
+                        italicized = format.italic;
+                    if (bolded && italicized) {
+                        newSpan.attr('id' , idNum);
+                        newSpan.html('<b><i>' + contentVal + '</i></b>');
+                        $("#editor").append(newSpan);
+                    } else if (bolded) {
+                        newSpan.attr('id' , idNum);
+                        newSpan.html('<b>' + contentVal + '</b>');
+                        $("#editor").append(newSpan);
+                    } else if (italicized) {
+                       newSpan.attr('id' , idNum);
+                        newSpan.html('<i>' + contentVal + '</i>');
+                        $("#editor").append(newSpan);
+                    } else {
+                            newSpan.attr('id' , idNum);
+                            newSpan.html(contentVal);
+                            $("#editor").append(newSpan);
+                           }
+                } else {
+                    newSpan.attr('id' , idNum + '_break');
+                    newSpan.html(' <br> ');
+                    $("#editor").append(newSpan);
+                }
+            });
+        
+            var lastSpan = $('<span></span>');
+            lastSpan.attr('id' , contents.length + 1 + '_last');
+            lastSpan.html('&nbsp;');
+            $("#editor").append(lastSpan);
+           
+            var letterSpans = $('#editor').children();
+            letterSpans.each(function () {
+                var letterSpan = $(this),
+                    selectionStart,
+                    selectionEnd,
+                    difference;
+                letterSpan.mousedown(function () {
+                    if ($('#cursor')) {
+                        $('#cursor').remove();
+                    }
                     letterSpans.css('background-color' , 'transparent');
-                    cursor = $('<span>|</span>');
-                    cursor.attr('id', 'cursor');
-                    cursor.css({
-                        'color': 'red',
-                        'font-weight': 'bold'
+                    letterSpans.off('mousemove');
+                    selectionStart = parseInt($(this).attr('id'));
+                    letterSpans.off('mousemove').mousemove(function (ev) {
+                        letterSpans.off('mouseup').mouseup(function () {
+                        selectionEnd = parseInt($(this).attr('id'));
+                            difference = selectionStart - selectionEnd;
+                            for(var ww = 0; ww < Math.abs(difference); ww++){
+                               var spans = $('span');
+                                if(difference > 0){
+                                   var redId = selectionEnd + ww;
+                                } else {
+                                   var redId = selectionStart + ww;     
+                                }
+                               var red = $('#'+ redId);
+                               red.css('background-color', 'red'); 
+                            };
+                            
+                            letterSpans.off('mousemove');
+                        });
                     });
-                    letterSpan.before(cursor);
-                    setInterval(function () {
-                        cursorBlink()
-                    }, 300);
+                    letterSpans.off('mouseup').mouseup(function () {
+                        letterSpans.css('background-color' , 'transparent');
+                        cursor = $('<span>|</span>');
+                        cursor.attr('id', 'cursor');
+                        cursor.css({
+                            'color': 'red',
+                            'font-weight': 'bold'
+                        });
+                        letterSpan.before(cursor);
+                        setInterval(function () {
+                            cursorBlink()
+                        }, 300);
+                    });
                 });
             });
-        });
+        };
     };
 
     render();
